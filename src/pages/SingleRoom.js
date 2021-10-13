97092acc90f75134b014d99d467b154316133c41
Shahzaib <44567763+shahzaibg786@users.noreply.github.com>
2021-10-13 12:34:11 -0700

    single room modification

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -3,22 +3,44 @@ import defaultBcg from '../images/room-1.jpeg'
 import Banner from '../components/Banner'
 import { Link } from 'react-router-dom'
 import { RoomContext } from '../context'
+import Minibanner from '../components/Minibanner'
 
 export default class SingleRoom extends Component {
 
     constructor(props){
         super(props)
-        console.log(this.props)
+        // console.log(this.props)
+        this.state = {
+            slug: this.props.match.params.slug,
+            defaultBcg
+        }
     }
 
+    static contextType = RoomContext
     componentDidMount(){
 
     }
     render() {
-        return (
-            <div>
-                This is Single Room.
+        const {getRoom} = this.context
+        const room = getRoom(this.state.slug)
+        if(!room){
+            return (
+            <div className="error">
+                <h3>No Such Rooms Available ...</h3>
+                <Link to="/rooms" className="btn-primary">
+                Back to Rooms
+                </Link>
             </div>
+            )
+        }
+
+        const {name,description,capacity,size,price,extras,breakfast,pets,images} = room
+        return (
+            <Banner className="roomsHero">
+                <Minibanner title={`${name} room`}>
+                    <Link to="/rooms" className="btn-primary">Back to Rooms</Link>
+                </Minibanner>
+            </Banner>
         )
     }
 }
