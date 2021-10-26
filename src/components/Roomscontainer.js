6b42e2e2f0a03a81ec09c82351c45330a24bf0f0
Shahzaib <44567763+shahzaibg786@users.noreply.github.com>
2021-10-26 14:01:59 -0700

    Pagination module added
    In this branch, I have used react paginate library to implement pagination functionality.

--- src/components/Roomscontainer.js
+++ src/components/Roomscontainer.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import RoomList from "./RoomList";
 import Roomsfilter from "./Roomsfilter";
 import { withRoomConsumer } from "../context";
 import Loading from "./Loading";
+import { Pagination } from "./Pagination";
+
 
 function Roomcontainer({ context }) {
   const  {loading, sortedRooms, rooms}  = context;
+
+  const [currentPage, setCurrentPage] = useState(0)
+  // const [roomsPerPage, setRoomsPerPage] = useState(4)
+  const roomsPerPage = 8
+//get current rooms
+ const indexOfLastRoom = currentPage * roomsPerPage
+//  const indexOfFirstRoom = indexOfLastRoom - roomsPerPage
+// const currentRooms = rooms.slice(indexOfFirstRoom, indexOfLastRoom)
+//  const currentRooms = rooms.slice(indexOfFirstRoom, indexOfLastRoom)
+const currentRooms = sortedRooms.slice(indexOfLastRoom, indexOfLastRoom + roomsPerPage)
+console.log(currentRooms)
+
   if (loading) {
     return <Loading />;
   }
   return (
     <>
       <Roomsfilter rooms={rooms} />
-      <RoomList rooms={sortedRooms} />
+      {/* <RoomList rooms={sortedRooms} /> */}
+      <RoomList rooms={currentRooms} />
+      <Pagination roomsPerPage={roomsPerPage} setCurrentPage={setCurrentPage} totalRooms={rooms.length}/>
     </>
   );
 }
